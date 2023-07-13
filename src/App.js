d804fdd1384e19ec034442385b8ec3b87934b127
mbdesigns1989 <mbdesigns1989@yahoo.com>
2023-07-13 18:54:24 +0300

    Added header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Cars from './components/Car/Cars';
 import AddCar from './components/Car/AddCars';
 import DeleteCar from './components/Car/DeleteCar/DeleteCar';
+import Header from './components/Header';
+
 
 function App() {
   return (
@@ -10,15 +12,11 @@ function App() {
         <div><Header /></div>
         <div>
           <Routes>
-            <Route exact path="/" element={<Home />} />
+ 
             <Route exact path="/cars" element={<Cars />} />
-            <Route exact path="/signup" element={<Signup />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/new_reservations" element={<ReservationNew />} />
-            <Route exact path="/reservedCars" element={<ReservedCars />} />
             <Route exact path="/addcars" element={<AddCar />} />
             <Route exact path="/deleteCar" element={<DeleteCar />} />
-            <Route exact path="cars/:id" element={<CarDetails />} />
+            
           </Routes>
         </div>
       </div>
